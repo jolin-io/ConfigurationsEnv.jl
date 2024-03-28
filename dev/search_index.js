a7f73b9670b5806dbfe473f0bf6d985d31f8969e
Documenter.jl <documenter@juliadocs.github.io>
2024-03-28 11:14:43 +0000

    build based on 5256c55

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConfigurationsENV","category":"page"},{"location":"#ConfigurationsENV","page":"Home","title":"ConfigurationsENV","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConfigurationsENV.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConfigurationsENV]","category":"page"}]
+}
